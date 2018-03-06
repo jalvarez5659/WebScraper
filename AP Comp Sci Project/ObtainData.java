8034f4aafafb411d3372b55904f8f26c7473c5a8
summit <summit@hs-90534-103.lan.summit.k12.nj.us>
2018-03-06 08:42:30 -0500

    work u bitch

--- AP Comp Sci Project/ObtainData.java
+++ AP Comp Sci Project/ObtainData.java
@@ -1,8 +1,38 @@
-//import
+import java.net.*;
+import java.io.*;
+
 public class ObtainData
 {
     public ObtainData()
     {
-        
+
+    }
+
+    public static void main()
+    {
+        /*try{
+            URL oracle = new URL("https://www.oracle.com/index.html");
+            BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+            String inputLine;
+            while((inputLine = in.readLine()) != null)
+            {
+                System.out.println(inputLine);
+            }
+            in.close();
+        }catch(IOException e){
+            System.out.println("Didn't work");
+        }*/
+        try{
+            URL oracle = new URL("https://www.oracle.com/index.html");
+            BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+            String inputLine;
+            while((inputLine = in.readLine()) != null)
+            {
+                System.out.println(inputLine);
+            }
+            in.close();
+        }catch(IOException e){
+            System.out.println("Didn't work");
+        }
     }
 }
